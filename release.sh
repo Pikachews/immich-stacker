d1ef664c9770ea6f536968e96e5e3e5b139bd339
Pikachews <Pikachews@users.noreply.github.com>
2024-06-27 22:20:08 +0000

    Update release script to publish to personal packages

--- release.sh
+++ release.sh
@@ -2,12 +2,12 @@
 
 version=$(git describe --tags)
 
-podman build . -t ghcr.io/mattdavis90/immich-stacker:${version}
-podman build . -t ghcr.io/mattdavis90/immich-stacker:latest
-podman build . -t mattdavis90/immich-stacker:${version}
-podman build . -t mattdavis90/immich-stacker:latest
+# podman build . -t ghcr.io/pikachews/immich-stacker:${version}
+# podman build . -t ghcr.io/pikachews/immich-stacker:latest
+podman build . -t pikachews/immich-stacker:${version}
+podman build . -t pikachews/immich-stacker:latest
 
-podman push ghcr.io/mattdavis90/immich-stacker:${version}
-podman push ghcr.io/mattdavis90/immich-stacker:latest
-podman push mattdavis90/immich-stacker:${version}
-podman push mattdavis90/immich-stacker:latest
+# podman push ghcr.io/pikachews/immich-stacker:${version}
+# podman push ghcr.io/pikachews/immich-stacker:latest
+podman push pikachews/immich-stacker:${version}
+podman push pikachews/immich-stacker:latest
